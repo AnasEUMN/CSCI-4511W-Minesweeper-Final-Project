942ebbe28ead268b366fad32a51e52778d36d187
Evan Dieterle <diete079@umn.edu>
2024-12-11 14:14:52 -0600

    Updated parameters

--- search.py
+++ search.py
@@ -16,33 +16,32 @@ def heuristic_4(state):
 class Minesweeper():
     def __init__(self, initial):
         """ Define initial state """
-        self.initial = initial
+        self.state = initial
     
-    def best_action(self, state, heuristic):
+    def best_action(self, heuristic):
         """ Given a Minesweeper state, return the best action according to the given Minesweeper heuristic """
         if heuristic == 1:
-            best_action = heuristic_1(state)
+            best_action = heuristic_1(self.state)
         elif heuristic == 2:
-            best_action = heuristic_2(state)
+            best_action = heuristic_2(self.state)
         elif heuristic == 3:
-            best_action = heuristic_3(state)
+            best_action = heuristic_3(self.state)
         elif heuristic == 4:
-            best_action = heuristic_4(state)
+            best_action = heuristic_4(self.state)
         return best_action 
 
-    def goal_test(self, state):
+    def goal_test(self):
         """ Given a state, return True if state is a goal state or False, otherwise """
-        return state.game_over()
+        return self.state.game_over()
     
     def play_game(self, heuristic):
         """ Game loop search specific for a Minesweeper problem. This uses a heuristic to test which cell to reveal before taking actions.
         In Minesweeper, once an action is taken, there is no going back. """
-        state = self.initial
         moves = 0
-        while not self.goal_test(state):
-            action = self.best_action(state, heuristic)
+        while not self.goal_test(self.state):
+            action = self.best_action(self.state, heuristic)
             cell = action.split(",")
-            win = state.reveal_cell(int(cell[0]), int(cell[1]), False)
+            win = self.state.reveal_cell(int(cell[0]), int(cell[1]), False)
             moves += 1
-        return (state, win, moves)   
+        return (self.state, win, moves)   
     
