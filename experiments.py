fec2e391f365084f374055ef584582869fe224ea
Evan Dieterle <diete079@umn.edu>
2024-12-12 17:04:30 -0600

    Made testing loop

--- experiments.py
+++ experiments.py
@@ -1,25 +1,13 @@
 from minesweeper import *
 
-# initial_state = Minefield(4, 4, 3)
-# minesweeper = Minesweeper(initial_state)
-# heuristic = 3
-# result = minesweeper.play_game(heuristic)
+def test_minesweeper_heuristic(heuristic):
+    wins = 0
+    for i in range(1000):
+        initial_state = Minefield(4, 4, 3)
+        minesweeper = Minesweeper(initial_state)
+        result = minesweeper.play_game(heuristic) 
+        if result[1] == True:
+            wins += 1
+    print(f"Wins: {wins}")  
 
-# final_state = result[0]
-# win = result[1]
-# moves = result[2]
-
-# print(final_state)
-# print(f"Win: {win}")
-# print(f"Moves: {moves}")
-# print()
-
-wins = 0
-for i in range(1000):
-    initial_state = Minefield(4, 4, 3)
-    minesweeper = Minesweeper(initial_state)
-    heuristic = 3
-    result = minesweeper.play_game(heuristic) 
-    if result[1] == True:
-        wins += 1
-print(wins)   
+test_minesweeper_heuristic(1)
