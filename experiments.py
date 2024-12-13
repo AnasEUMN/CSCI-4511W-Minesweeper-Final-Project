44b4521e523c110a136ec815af45a8be1aa3c112
Evan Dieterle <diete079@umn.edu>
2024-12-13 11:13:22 -0600

    Fixed test loop

--- experiments.py
+++ experiments.py
@@ -2,13 +2,16 @@ from minesweeper import *
 
 def test_minesweeper_heuristic(heuristic):
     wins = 0
-    for i in range(1000):
-        initial_state = Minefield(4, 4, 3)
+    moves = 0
+    for i in range(100):
+        initial_state = Minefield(5, 5, 4)
         minesweeper = Minesweeper(initial_state)
         result = minesweeper.play_game(heuristic) 
         print(result[0])
         if result[1]:
             wins += 1
-    print(f"Wins: {wins}")  
+        moves += result[2]
+    print(f"Wins: {wins}") 
+    print(f"Moves: {moves}") 
 
-test_minesweeper_heuristic(4)
+test_minesweeper_heuristic(2)
